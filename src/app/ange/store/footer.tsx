ebe6fe6af4e966e8c296067d2d78de1bdcd4aa4e
aoyama-micron <j.aoyama@micron-inc.co.jp>
2024-01-26 10:23:08 +0900

    ✨1/26 10:25 更新

--- src/app/ange/store/footer.tsx
+++ src/app/ange/store/footer.tsx
@@ -4,11 +4,8 @@ import React from "react";
 const Footer: React.FC = () => {
 	return (
 		<>
-			<div className="relative h-auto w-full">
-				<Image className="image-fit" src={`/images/_footer_dummy.png`} alt="フッターダミー" fill />
-			</div>
 			<footer className="fixed bottom-0 left-0 right-0 z-50 w-full border-t border-gray-300">
-				<Image className="image-fit" src={`/images/_footer.png`} alt="フッター" fill />
+				<Image className="image-fit" src={`/images/_footer_store.png`} alt="フッター" fill />
 			</footer>
 		</>
 	);
